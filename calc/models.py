8d208a173d3bccec275f34fb7bb226a25fffe9b5
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-18 10:50:27 +0545

    models

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Calculation(models.Model):
+    num1 = models.FloatField()
+    num2 = models.FloatField()
+    operation = models.CharField(max_length=20)
+    result = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.num1} {self.operation} {self.num2} = {self.result}"
