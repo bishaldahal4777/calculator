6f28a091b42f348f6064b21a22afaf50ffaac2c2
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-19 11:13:09 +0545

    new commit

--- calc/views.py
+++ calc/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Calculation
 
-
+def index(request):
+    # Fetch recent history
+    history = Calculation.objects.all().order_by('-created_at')[:5]
+    return render(request, "calc/index.html", {"history": history})
 
 def calculate(request):
     if request.method == "POST":
